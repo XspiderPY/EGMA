8c71c1704d472b286b98901b6fbb63008f907278
XspiderPY <48117479+XspiderPY@users.noreply.github.com>
2019-07-21 17:12:35 +0200

    Add files via upload

---
+++ connect.sh
@@ -0,0 +1,278 @@
+#Colors
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+lightgreen='\e[1;32m'
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+clear
+function pa()
+{
+    echo -e """	
+         ++++++++++++++++++++++++++++++++++++++++++++
+        +##########################################+
+       +#      welcome to $red EGMA$nc massenger        #+
+       +#                            $blue v1.0.0$nc     #+
+      +##########################################+
+     ++++++++++++++++++++++++++++++++++++++++++++ 
+$lightgreen dev. by$nc $red xspider$nc🇸🇩️  
+              ╔──────────────────────────────╗ 
+              |   Encrypted massenger v1.0.0 |  
+              ╚──────────────────────────────╝ 
+ [*] Thanks For Using EGMA encrypted massanger:)
+"""
+}
+function qw()
+{
+    echo "connecting to server"
+echo "----------------------------"
+}
+function as()
+{
+    echo "----------------------------"
+sleep 0.9
+clear
+}
+function a0()
+{
+echo -e "█                          |"  "$red 0% $nc"
+}
+function a5()
+{
+echo -e "██                         |"  "$red 5%$nc"
+}
+function a10()
+{
+echo -e "███                        |"  "$red 10%$nc"
+}
+function a15()
+{
+echo -e "████                       |"  "$red 15%$nc"
+}
+function a20()
+{
+echo -e "█████                      |"  "$red 20%$nc"
+}
+function a25()
+{
+echo -e "██████                     |"  "$red 25%$nc"
+}
+function a30()
+{
+echo -e "████████                   |"  "$red 30%$nc"
+}
+function a35()
+{
+echo -e "█████████                  |"  "$red 35$nc"
+}
+function a40()
+{
+echo -e "██████████                 |"  "$red 40%$nc"
+}
+function a45()
+{
+echo -e "███████████                |"  "$red 45%$nc"
+}
+function a50()
+{
+echo -e "████████████               |"  "$red 50%$nc"
+}
+function a55()
+{
+echo -e "█████████████              |"  "$red 55%$nc"
+}
+function a60()
+{
+echo -e "██████████████             |"  "$red 60%$nc"
+}
+function a65()
+{
+echo -e "███████████████            |"  "$red 65%$nc"
+}
+function a70()
+{
+echo -e "████████████████           |"  "$red 70%$nc"
+}
+function a75()
+{
+echo -e "█████████████████          |"  "$red 75%$nc"
+}
+function a80()
+{
+echo -e "██████████████████         |"  "$red 80%$nc"
+}
+function a85()
+{
+echo -e "███████████████████        |"  "$red 85%$nc"
+}
+function a90()
+{
+echo -e "████████████████████       |"  "$red 90%$nc"
+}
+function a95()
+{
+echo -e "█████████████████████      |"  "$red 95%$nc"
+}
+function a100()
+{
+echo -e "███████████████████████████|"  "$yellow 100%$nc"
+
+}
+#
+pa
+qw
+a0
+as
+#
+#
+pa
+qw
+a5
+as
+#
+#
+pa
+qw
+a10
+as
+#
+#
+pa
+qw
+a15
+as
+#
+#
+pa
+qw
+a20
+as
+#
+#
+pa
+qw
+a25
+as
+#
+#
+pa
+qw
+a30
+as
+#
+#
+pa
+qw
+a30
+as
+#
+#
+pa
+qw
+a35
+as
+#
+#
+pa
+qw
+a40
+as
+#
+#
+pa
+qw
+a45
+as
+#
+#
+pa
+qw
+a50
+as
+#
+#
+pa
+qw
+a55
+as
+#
+#
+pa
+qw
+a60
+as
+#
+#
+pa
+qw
+a65
+as
+#
+#
+pa
+qw
+a70
+as
+#
+#
+pa
+qw
+a75
+as
+#
+#
+pa
+qw
+a80
+as
+#
+#
+pa
+qw
+a85
+as
+#
+#
+pa
+qw
+a90
+as
+#
+#
+pa
+qw
+a95
+as
+#
+#
+pa
+qw
+a100
+as
+#
+pa
+clear
+echo -e $blue  "-------------------"
+echo -e $green "[ ✔ ] server found."
+echo -e $blue  "-------------------"$nc
+sleep 2
+echo " #############################"
+echo -e "$blue [ ✔ ] connected to server.............................$nc "
+sleep 1.5
+echo -e "$yellow chat room testing$nc $yellow ..................$nc "
+sleep 2
+echo " chat room working[ ✔ ]"
+sleep 2.8
+clear 
+
+clear 
+pa
+echo -e "$blue Now you can start chatting...............$nc "
+
---
+++ error.sh
@@ -0,0 +1,25 @@
+#code by xspider 
+#coded with love in sudan
+clear
+#Colors
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+lightgreen='\e[1;32m'
+yellow="\033[1;33m"
+Purple="\033[0;35m"
+Cyan="\033[0;36m"
+Cafe="\033[0;33m"
+Fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+echo -e """
+
+     $red    ▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪
+         ▪ error access_denied ▪
+         ▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪$nc
+  
+    $red[ X ]$nc chat room or host server $red->$nc $red not found!$nc 
+      """
